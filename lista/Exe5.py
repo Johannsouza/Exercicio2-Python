8b033f4e0606951f7b9759829d73a19004412715
Johannsouza <112652745+Johannsouza@users.noreply.github.com>
2025-02-21 20:09:04 -0300

    Exe5.py

--- lista/Exe5.py
+++ lista/Exe5.py
@@ -2,15 +2,11 @@
 # verificar e informar se os valores formam um triângulo válido (a soma de dois lados deve ser
 # sempre maior que o terceiro).
 
-lado1=int(input("Digite o valor do primeiro lado do triângulo: "))
-lado2=int(input("Digite o valor do segundo lado do triângulo: "))
-lado3=int(input("Digite o valor do terceiro lado do triângulo: "))
+lado1 = int(input("Digite o valor do primeiro lado do triângulo: "))
+lado2 = int(input("Digite o valor do segundo lado do triângulo: "))
+lado3 = int(input("Digite o valor do terceiro lado do triângulo: "))
 
-if(lado1+lado2>lado3):
-    print("O triângulo é válido!")
-elif(lado1+lado3>lado2):
-    print("O triângulo é válido!")
-elif(lado2+lado3>lado1):
+if lado1 + lado2 > lado3 and lado1 + lado3 > lado2 and lado2 + lado3 > lado1:
     print("O triângulo é válido!")
 else:
     print("O triângulo não é válido!")
