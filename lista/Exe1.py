480d279d7145d1d2781fa15d0ac3eae230e8e0f9
Johannsouza <112652745+Johannsouza@users.noreply.github.com>
2025-02-21 20:01:07 -0300

    Exe1.py

---
+++ lista/Exe1.py
@@ -0,0 +1,8 @@
+# 1 - Escreva um programa que solicite um número inteiro ao usuário e informe se ele é par ou ímpar.
+
+# x = int(input("Digite um Número inteiro: "))
+
+# if(x%2==0):
+#     print("O número é par")
+# else:
+#     print("O número é ímpar")
