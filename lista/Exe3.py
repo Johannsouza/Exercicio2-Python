dcdee1dbd5979277bb288e6f49acdddca2491050
Johannsouza <112652745+Johannsouza@users.noreply.github.com>
2025-02-21 20:01:50 -0300

    Exe3.py

---
+++ lista/Exe3.py
@@ -0,0 +1,14 @@
+# 3 - Crie um programa que peça a idade de uma pessoa e informe sua classificação:
+# • Menor de idade (menos de 18 anos)
+# • Adulto (entre 18 e 59 anos)
+# • Idoso (60 anos ou mais)
+
+
+idade = int(input("Digite a sua idade: "))
+
+if(idade<18):
+    print("Menor de Idade!")
+elif(idade>=18 or idade<60):
+    print("Adulto!")
+else:
+    print("Idoso!")
